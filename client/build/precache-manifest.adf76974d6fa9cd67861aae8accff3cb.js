2fc3fb98b3694e801477f3205928989095b82b31
rishijain07 <78855205+rishijain07@users.noreply.github.com>
2022-07-16 01:52:43 +0530

    commit message

---
+++ client/build/precache-manifest.adf76974d6fa9cd67861aae8accff3cb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "379bb12481f3a32d1bbaf0066ec0b9fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3aee43e56397629d2f37",
+    "url": "/static/css/main.c27eb8cf.chunk.css"
+  },
+  {
+    "revision": "b9ef8f0112274fe3697d",
+    "url": "/static/js/2.e777eac3.chunk.js"
+  },
+  {
+    "revision": "88bc88fae9bdbe40a2ebfacf05fe1d41",
+    "url": "/static/js/2.e777eac3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3aee43e56397629d2f37",
+    "url": "/static/js/main.8969939f.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "bad931c0ebb4abd622746448b4e330c2",
+    "url": "/static/media/hostel.bad931c0.jpg"
+  },
+  {
+    "revision": "8f72d2e989276e9dfb16a098afaf9410",
+    "url": "/static/media/student.8f72d2e9.jpg"
+  }
+]);
